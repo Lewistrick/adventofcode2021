33334c03f7fc331d9a985fc405887025edc2d348
Lewistrick <erickwilts@gmail.com>
2021-12-09 10:42:27 +0100

    More comments!

--- day09.py
+++ day09.py
@@ -1,16 +1,19 @@
 from functools import reduce
 
+# read the input as a 2D matrix of ints
 grid = []
 with open("in09.txt") as lines:
     for line in lines:
         grid.append([int(i) for i in line.strip()])
 
+# define directions in which neighbors are found: left, up, right, down
 directions = [(-1,0), (0,-1), (1,0), (0,1)]
 
-# find lowest points
+## part 1
 lowpoints = []
 for x, row in enumerate(grid):
     for y, val in enumerate(row):
+        # find neighbors
         to_check = []
         for dx, dy in directions:
             if x + dx < 0 or y + dy < 0:
@@ -18,11 +21,15 @@ for x, row in enumerate(grid):
             if x + dx >= len(grid) or y + dy >= len(row):
                 continue
             to_check.append(grid[x+dx][y+dy])
+        # check if all neighbors have higher values
         if all(val < check for check in to_check):
             lowpoints.append(val)
+# risk level is `sum(height + 1)` for every lowpoint
+# which is the same as `sum(height) + N` (taking the 1 out of the sum)
 print("part1:", sum(lowpoints) + len(lowpoints))
 
-# part 2
+## part 2
+# find all coordinates that have no 9 in them; they're all part of a basin
 to_check = {(x, y)
             for x in range(len(grid))
             for y in range(len(grid[0]))
@@ -52,6 +59,8 @@ while to_check:
     basins.append(curr_basin)
     curr_basin = []
 
+# sort the basins by size and select the last 3
 biggest_basins = sorted(basins, key=len)[-3:]
+# take the product of all the sizes
 part2 = reduce(int.__mul__, (len(basin) for basin in biggest_basins))
 print("part 2:", part2)
